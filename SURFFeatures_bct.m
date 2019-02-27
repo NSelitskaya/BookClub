8ea5cd4afa79bdc4a1120d0d1dda1a80eb938a4f
Natalya Selitskaya <n_selitskaya@yahoo.com>
2019-02-26 23:13:08 -0500

    Add files via upload

--- SURFFeatures_bct.m
+++ SURFFeatures_bct.m
@@ -3,8 +3,8 @@ clear all; close all; clc;
 
 %% Dataset root folder template and suffix
 dataFolderTmpl = '~/data/BC1_Sfx';
-%dataFolderSfx = '96x64';
-dataFolderSfx = '1072x712';
+dataFolderSfx = '96x64';
+%dataFolderSfx = '1072x712';
 
 
 % Create imageDataset of all images in selected baseline folders
@@ -77,12 +77,10 @@ for i=1:nMakeups
     mkTable{i,1} = testDataSetFolders(i);
     mkTable{i,2} = meanMkAcc;
     
-    %%
-    [~, ~] = splitPredictions(testSets{i}, categoryClassifier, predictedLabelIdx, score);
-    
 
-    %%
-    [tn, ~] = size(testSets{i}.Files);
+    %% Compute a row of the Confusion matrix.
+    %  For current test set against all class labels
+    [nFiles, ~] = size(testSets{i}.Files);
     
     meanMkConf = zeros(1, nClasses);
      
@@ -92,7 +90,7 @@ for i=1:nMakeups
     j = 1;   
     for j = 1:nClasses
 
-        tmpStr = strings(tn,1);
+        tmpStr = strings(nFiles,1);
         tmpStr(:) = string(labels(j));
     
         meanMkConf(j) = mean(string(categoryClassifier.Labels(predictedLabelIdx)') == tmpStr);
@@ -108,6 +106,24 @@ for i=1:nMakeups
     mkTable{i,3} = maxAccCat;
     mkTable{i,4} = maxAcc;
     
+    
+    %% Split test dataset into images that were identified correctly and incorrectly
+    [rightTestFiles, ~] = splitPredictions(testSets{i}, categoryClassifier, predictedLabelIdx, score);
+    
+    
+    %% Find a pair of the correctly identified test and training images with maximum matched features
+    [nFiles, ~] = size(rightTestFiles);
+    
+    [mFiles, ~] = size(trainingSet.Files);
+    tmpStr = strings(mFiles,1);
+    tmpStr(:) = string(rightTestFiles{1}(2));
+    
+    rightTrainFiles = trainingSet.Files( string(trainingSet.Labels) == tmpStr );
+        
+    %for k=1:nFiles
+        
+    %end
+    
 end
 
 %% Results
@@ -116,248 +132,60 @@ cell2table(mkTable, 'VariableNames', varNames)
 
 
 
-%%
-mkDataSetFolder = strings(0);
-mkLabel = strings(0);
-
-[tmpStr, ~] = strsplit(dataSetFolder(1), '/');
-[~, c] = size(tmpStr);
-mkLabelCur = tmpStr(1,c);
-%mkDataSetFolder = [mkDataSetFolder, 'S1_1072x712/S1GL1_1072x712'];
-%mkLabel = [mkLabel, mkLabelCur];
-mkDataSetFolder = [mkDataSetFolder, 'S1_1072x712/S1NM1_1072x712'];
-mkLabel = [mkLabel, mkLabelCur]; 
-mkDataSetFolder = [mkDataSetFolder, 'S1_1072x712/S1MK1_1072x712'];
-mkLabel = [mkLabel, mkLabelCur]; 
-mkDataSetFolder = [mkDataSetFolder, 'S1_1072x712/S1MK2_1072x712'];
-mkLabel = [mkLabel, mkLabelCur]; 
-mkDataSetFolder = [mkDataSetFolder, 'S1_1072x712/S1MK3_1072x712'];
-mkLabel = [mkLabel, mkLabelCur]; 
-mkDataSetFolder = [mkDataSetFolder, 'S1_1072x712/S1MK4_1072x712'];
-mkLabel = [mkLabel, mkLabelCur];
-mkDataSetFolder = [mkDataSetFolder, 'S1_1072x712/S1MK5_1072x712'];
-mkLabel = [mkLabel, mkLabelCur];
-mkDataSetFolder = [mkDataSetFolder, 'S1_1072x712/S1MK6_1072x712'];
-mkLabel = [mkLabel, mkLabelCur];
-
-
-%[tmpStr, ~] = strsplit(dataSetFolder(2), '/');
-%[~, c] = size(tmpStr);
-%mkLabelCur = tmpStr(1,c);
-%mkDataSetFolder = [mkDataSetFolder, 'S2_1072x712/S2GL1_1072x712'];
-%mkLabel = [mkLabel, mkLabelCur];
-%mkDataSetFolder = [mkDataSetFolder, 'S2_1072x712/S2HD1_1072x712'];
-%mkLabel = [mkLabel, mkLabelCur];
-
-
-[tmpStr, ~] = strsplit(dataSetFolder(3), '/');
-[~, c] = size(tmpStr);
-mkLabelCur = tmpStr(1,c);
-%mkDataSetFolder = [mkDataSetFolder, 'S3_1072x712/S3HD1_1072x712'];
-%mkLabel = [mkLabel, mkLabelCur];
-%mkDataSetFolder = [mkDataSetFolder, 'S3_1072x712/S3HD2_1072x712'];
-%mkLabel = [mkLabel, mkLabelCur];
-mkDataSetFolder = [mkDataSetFolder, 'S3_1072x712/S3NM1_1072x712'];
-mkLabel = [mkLabel, mkLabelCur]; 
-mkDataSetFolder = [mkDataSetFolder, 'S3_1072x712/S3MK1_1072x712'];
-mkLabel = [mkLabel, mkLabelCur]; 
-mkDataSetFolder = [mkDataSetFolder, 'S3_1072x712/S3MK2_1072x712'];
-mkLabel = [mkLabel, mkLabelCur];
-mkDataSetFolder = [mkDataSetFolder, 'S3_1072x712/S3MK3_1072x712'];
-mkLabel = [mkLabel, mkLabelCur];
-mkDataSetFolder = [mkDataSetFolder, 'S3_1072x712/S3MK4_1072x712'];
-mkLabel = [mkLabel, mkLabelCur];
-
-
-[tmpStr, ~] = strsplit(dataSetFolder(4), '/');
-[~, c] = size(tmpStr);
-mkLabelCur = tmpStr(1,c);
-mkDataSetFolder = [mkDataSetFolder, 'S4_1072x712/S4MK1_1072x712'];
-mkLabel = [mkLabel, mkLabelCur];
-%mkDataSetFolder = [mkDataSetFolder, 'S4_1072x712/S4GL1_1072x712'];
-%mkLabel = [mkLabel, mkLabelCur];
-%mkDataSetFolder = [mkDataSetFolder, 'S4_1072x712/S4HD1_1072x712'];
-%mkLabel = [mkLabel, mkLabelCur];
-
-
-[tmpStr, ~] = strsplit(dataSetFolder(5), '/');
-[~, c] = size(tmpStr);
-mkLabelCur = tmpStr(1,c);
-mkDataSetFolder = [mkDataSetFolder, 'S5_1072x712/S5NM1_1072x712'];
-mkLabel = [mkLabel, mkLabelCur];
-mkDataSetFolder = [mkDataSetFolder, 'S5_1072x712/S5NM3_1072x712'];
-mkLabel = [mkLabel, mkLabelCur];
-mkDataSetFolder = [mkDataSetFolder, 'S5_1072x712/S5MK1_1072x712'];
-mkLabel = [mkLabel, mkLabelCur];
-mkDataSetFolder = [mkDataSetFolder, 'S5_1072x712/S5MK2_1072x712'];
-mkLabel = [mkLabel, mkLabelCur];
-mkDataSetFolder = [mkDataSetFolder, 'S5_1072x712/S5MK3_1072x712'];
-mkLabel = [mkLabel, mkLabelCur];
-
-
-[tmpStr, ~] = strsplit(dataSetFolder(6), '/');
-[~, c] = size(tmpStr);
-mkLabelCur = tmpStr(1,c);
-mkDataSetFolder = [mkDataSetFolder, 'S6_1072x712/S6MK1_1072x712'];
-mkLabel = [mkLabel, mkLabelCur];
-mkDataSetFolder = [mkDataSetFolder, 'S6_1072x712/S6MK2_1072x712'];
-mkLabel = [mkLabel, mkLabelCur];
-
-
-[tmpStr, ~] = strsplit(dataSetFolder(7), '/');
-[~, c] = size(tmpStr);
-mkLabelCur = tmpStr(1,c);
-mkDataSetFolder = [mkDataSetFolder, 'S7_1072x712/S7NM1_1072x712'];
-mkLabel = [mkLabel, mkLabelCur];
-mkDataSetFolder = [mkDataSetFolder, 'S7_1072x712/S7MK1_1072x712'];
-mkLabel = [mkLabel, mkLabelCur];
-mkDataSetFolder = [mkDataSetFolder, 'S7_1072x712/S7MK2_1072x712'];
-mkLabel = [mkLabel, mkLabelCur];
-
-
-%[tmpStr, ~] = strsplit(dataSetFolder(10), '/');
-%[~, c] = size(tmpStr);
-%mkLabelCur = tmpStr(1,c);
-%mkDataSetFolder = [mkDataSetFolder, 'S10_1072x712/S10MK1_1072x712'];
-%mkLabel = [mkLabel, 'S10NM1_1072x712'];
-%mkDataSetFolder = [mkDataSetFolder, 'S10_1072x712/S10MK2_1072x712'];
-%mkLabel = [mkLabel, 'S10NM1_1072x712'];
-%mkDataSetFolder = [mkDataSetFolder, 'S10_1072x712/S10MK3_1072x712'];
-%mkLabel = [mkLabel, 'S10NM1_1072x712'];
-%mkDataSetFolder = [mkDataSetFolder, 'S10_1072x712/S10MK4_1072x712'];
-%mkLabel = [mkLabel, 'S10NM1_1072x712'];
-
-
-%[tmpStr, ~] = strsplit(dataSetFolder(11), '/');
-%[~, c] = size(tmpStr);
-%mkLabelCur = tmpStr(1,c);
-%mkDataSetFolder = [mkDataSetFolder, 'S11_1072x712/S11MK1_1072x712'];
-%mkLabel = [mkLabel, 'S11NM1_1072x712'];
-%mkDataSetFolder = [mkDataSetFolder, 'S11_1072x712/S11MK2_1072x712'];
-%mkLabel = [mkLabel, 'S11NM1_1072x712'];
-
-
-[~, nMakeups] = size(mkDataSetFolder);
-
-
-%%
-i = 2;
-for i=1:nMakeups
-    
-    mkDataSetFullFolder = fullfile(dataFolder, mkDataSetFolder(i));
-     
-    %% Load Image Information from Makup Face Database
-    mkImages = imageDatastore(mkDataSetFullFolder, 'IncludeSubfolders', false,...
-                                'LabelSource', 'none');
-    mkImages.ReadFcn = @readFunctionGray_n;
-   
-    % Label images                        
-    [n, ~] = size(mkImages.Files);  
-    tmpStr = strings(n,1);
-    tmpStr(:) = mkLabel(i);
-    mkImages.Labels = tmpStr; 
-                            
-    countEachLabel(mkImages);
-                          
-    
-    % Split Database into Training & Test Sets in the ration 80% to 20%
-    %[mkTrainingSet, mkTestSet] = splitEachLabel(mkImages, 0.8, 'randomize');
-    mkTestSet = mkImages;
-   
-    %% Evaluate the classifier on the test set images and display the confusion matrix    
-    [predictedLabelIdx, score] = predict(categoryClassifier, mkTestSet);
-       
-    
-    %% Compute average accuracy
-    meanMkAcc = mean(string(categoryClassifier.Labels(predictedLabelIdx)') == string(mkTestSet.Labels));
-    
-    %details
-    %testLabels = categoryClassifier.Labels(predictedLabelIdx)';    
-    %M = [testSet.Files, testLabels]';
-    %fprintf("%s %s\n", M{:});
-
-    [tn, ~] = size(mkTestSet.Files);
-    meanMkConf = zeros(1, nClasses);
-
-    
-    maxAccCat = 0;
-    maxAcc = 0;
-    
-    j = 1;   
-    for j = 1:nClasses
-
-        tmpStr = strings(tn,1);
-        tmpStr(:) = string(categoryClassifier.Labels(j));
-    
-        meanMkConf(j) = mean(string(categoryClassifier.Labels(predictedLabelIdx)') == tmpStr);
-
-        %find the best category match
-        if maxAcc <= meanMkConf(j)
-            maxAccCat = tmpStr(j);
-            maxAcc = meanMkConf(j);
-        end
-        
-    end
-
-    %% Results
-    fprintf("Makeup Test accuracy: %f \n", meanMkAcc);
-    fprintf("Makeup Test Confusion\n");
-    fprintf(" %s ",string(categoryClassifier.Labels));
-    fprintf("\n %s", mkDataSetFolder(i)); meanMkConf
 
 
     
     %% Display poi and matches
-    k = 1;
-    img1 = readimage(mkTestSet, k);
+%    k = 1;
+%    img1 = readimage(mkTestSet, k);
     % Detect Features for First Image
-    img1Pts = detectSURFFeatures(img1);
-    [img1Features,  img1ValidPts] = extractFeatures(img1,  img1Pts);
+%    img1Pts = detectSURFFeatures(img1);
+%    [img1Features,  img1ValidPts] = extractFeatures(img1,  img1Pts);
 
-    figure; imshow(img1);
-    hold on; plot(img1Pts.selectStrongest(50));
+%    figure; imshow(img1);
+%    hold on; plot(img1Pts.selectStrongest(50));
 
 
     %% find one best match image from the best matching category
-    [n,~] = size(allImages.Files);
-    in_fl = 0;
-    index_pairs_max = 0;
-    for m = 1:n
+%    [n,~] = size(allImages.Files);
+%    in_fl = 0;
+%    index_pairs_max = 0;
+%    for m = 1:n
     
-        [img2t, info] = readimage(allImages, m);
-        if info.Label == maxAccCat
-            in_fl = 1;
-            img2Pts = detectSURFFeatures(img2t);
-            [img2Features,  ~] = extractFeatures(img2t,  img2Pts);
+ %       [img2t, info] = readimage(allImages, m);
+ %       if info.Label == maxAccCat
+ %           in_fl = 1;
+ %           img2Pts = detectSURFFeatures(img2t);
+ %           [img2Features,  ~] = extractFeatures(img2t,  img2Pts);
 
-            index_pairs = matchFeatures(img1Features, img2Features);
-            [index_pairs_n, ~] = size(index_pairs);
-            if index_pairs_n > index_pairs_max
-                img2 = img2t;
-                index_pairs_max = index_pairs_n;
-            end
-        else
-            if in_fl == 1
+ %           index_pairs = matchFeatures(img1Features, img2Features);
+ %           [index_pairs_n, ~] = size(index_pairs);
+ %           if index_pairs_n > index_pairs_max
+ %               img2 = img2t;
+ %               index_pairs_max = index_pairs_n;
+ %           end
+ %       else
+ %           if in_fl == 1
                 % done with best category
-                break
-            end
-        end
-    end
+ %               break
+ %           end
+ %       end
+ %   end
 
     %% display matching features
-    img2Pts = detectSURFFeatures(img2);
-    [img2Features,  img2ValidPts] = extractFeatures(img2,  img2Pts);
+%    img2Pts = detectSURFFeatures(img2);
+%    [img2Features,  img2ValidPts] = extractFeatures(img2,  img2Pts);
 
-    index_pairs = matchFeatures(img1Features, img2Features);
+%    index_pairs = matchFeatures(img1Features, img2Features);
 
-    img1Matched_pts = img1ValidPts(index_pairs(:,1)).Location;
-    img2Matched_pts = img2ValidPts(index_pairs(:,2)).Location;
+%    img1Matched_pts = img1ValidPts(index_pairs(:,1)).Location;
+%    img2Matched_pts = img2ValidPts(index_pairs(:,2)).Location;
 
-    figure, 
-    showMatchedFeatures(img1, img2, img1Matched_pts,... 
-                        img2Matched_pts, 'montage');
+%    figure, 
+%    showMatchedFeatures(img1, img2, img1Matched_pts,... 
+%                        img2Matched_pts, 'montage');
 
-end
+%end
 
 %dataSetFolder
 %fprintf("Training accuracy"); meanAccTrain
